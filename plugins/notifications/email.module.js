17a888337056c52a939abe2b78b233fcff426483
apostolidhs <john.apostolidh@gmail.com>
2019-03-03 21:25:22 +0200

    Add attachments

--- plugins/notifications/email.module.js
+++ plugins/notifications/email.module.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-KlarkModule(module, 'krkNotificationsEmail', function(_, q, $nodemailer) {
-
+KlarkModule(module, "krkNotificationsEmail", function(_, q, $nodemailer) {
   return {
     send: send
   };
 
   function send(opts, config) {
     if (!(config.emailSmtp && config.emailAddress)) {
-      throw new Error('Invalid arguments');
+      throw new Error("Invalid arguments");
     }
     var to = _.castArray(opts.to);
     var subject = opts.subject;
@@ -16,19 +15,23 @@ KlarkModule(module, 'krkNotificationsEmail', function(_, q, $nodemailer) {
 
     var transporter = $nodemailer.createTransport(config.emailSmtp);
 
-    var from = config.emailName ? '"' + config.emailName + '" <' + config.emailAddress + '>'
-                  : config.emailAddress;
+    var from = config.emailName
+      ? '"' + config.emailName + '" <' + config.emailAddress + ">"
+      : config.emailAddress;
 
     var mailOptions = {
-        from: from, // sender address
-        to: _.join(to, ', '), // list of receivers
-        subject: subject, // Subject line
-        html: content // html body
+      from: from, // sender address
+      to: _.join(to, ", "), // list of receivers
+      subject: subject, // Subject line
+      html: content // html body
     };
+
+    if (opts.attachments) {
+      mailOptions.attachments = opts.attachments;
+    }
 
     return q.promisify(function(cb) {
       return transporter.sendMail(mailOptions, cb);
     });
   }
-
 });
